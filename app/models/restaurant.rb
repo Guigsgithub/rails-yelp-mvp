070e928c0f7c3fc6237afe94f15b0ffbaef5d192
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-11 11:41:56 +0100

    Models completed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,6 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORIES }
+
+  has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  RATINGS = [1, 2, 3, 4, 5]
+  RATINGS = [0, 1, 2, 3, 4, 5]
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: RATINGS }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: RATINGS }
 end
